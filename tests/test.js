718e56fb4863bc0f7da0656e7ab6301a91e1aa04
Peter Furst <peter.furst@doclerholding.com>
2016-03-21 13:44:52 -0700

    2 Unknown amount of numbers added

--- tests/test.js
+++ tests/test.js
@@ -1,22 +1,24 @@
 var assert = require('chai').assert,
 	sinon = require('sinon'),
+	_ = require('underscore'),
 	StringCalculator = require('../libs/stringCalculator');
 
 suite('test project', function() {
 	setup(function() {
 		this.stringCalculator = new StringCalculator();
+		this._dataProvider = {
+			''              : 0,
+			'1'             : 1,
+			'0,1,2'         : 3,
+			'0,1,2,3,4,5'   : 15,
+			'21,12,30,43,5' : 111
+		};
 	});
 
-	test('test add method with empty string', function() {
-		assert.equal(this.stringCalculator.add(''), 0, 'empty string should result 0');
-	});
-
-	test('test addition with one number', function() {
-		assert.equal(this.stringCalculator.add('1'), 1, 'result should be 1');
-	});
-
-	test('test addition with two numbers', function() {
-		assert.equal(this.stringCalculator.add('1,2'), 3, 'result should be 3');
+	test('test multiple additions', function() {
+		_.each(this._dataProvider, function(testResult, testValue) {
+			assert.equal(this.stringCalculator.add(testValue), testResult, 'not equal');
+		}, this);
 	});
 
 	teardown(function() {
